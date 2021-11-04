a0a92ce6130c45a6a42ad8d59016b320f9dc8a8d
codeninjah <codeninjahblog@gmail.com>
2021-11-04 11:49:56 +0100

    Test grejer

--- problems/easy/even.js
+++ problems/easy/even.js
@@ -8,6 +8,7 @@ export const even = (xs) => {
 
 }
 
+
 const tests = [
 	[even, [[1, 2, 3, 4, 5]], [2, 4]],
 	[even, [[2, 4, 6]], [2, 4, 6]],
@@ -15,6 +16,7 @@ const tests = [
 	[even, [[9, 13, 17, 99]], []],
 	[even, [[0, 2, 5, 182391, 283182372]], [0, 2, 283182372]],
 ]
+
 
 export default tests
 
