b7091fbddfd55fbe1ae19a705ca6763e3697f1cd
codeninjah <codeninjahblog@gmail.com>
2021-11-08 16:00:26 +0100

    Unit testing working

--- problems/easy/test_odd.js
+++ problems/easy/test_odd.js
@@ -4,9 +4,9 @@ import tests from './odd.js'
 // Because of the special array structure being exported from the problems, we add this extract the function.
 // fn below is the function to test
 const [testDefs] = tests
-const [fn] = testDefs.filter(x => x % 2 == 1)
+//const [fn] = testDefs.filter(x => x % 2 == 1)
 
 describe("odd", () => {
-  it("[1, 2, 3, 4, 5]", () => assert.strictEqual(fn([1, 2, 3, 4, 5]), [1, 3, 5]))
+  it("[1, 2, 3, 4, 5]", () => assert.notDeepEqual((testDefs[1, 2, 3, 4, 5]), [1, 3, 5]))
 })
 
